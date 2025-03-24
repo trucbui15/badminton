217b2cbf27f4f6812dd25ef6b1ef93254e1c2602
TRÚC <cnttk44dbuithitruc0753@gmail.com>
2025-03-24 09:02:59 +0700

    update code file page

--- app/page.tsx
+++ app/page.tsx
@@ -95,7 +95,7 @@ export default function Home() {
             width={16}
             height={16}
           />
-          Go to nextjs.org → trucccccccc
+          Go to nextjs.org → truc bui
         </a>
       </footer>
     </div>
